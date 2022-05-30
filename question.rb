42be3ad84b283d4d79807ef6f213633671e6fb2f
icoolww <williamwijaya.mail@gmail.com>
2022-05-30 18:50:25 +0000

    Adding question

---
+++ question.rb
@@ -0,0 +1,15 @@
+class Question
+  attr_reader :question
+
+  def initialize
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    @answer = num1 + num2
+    @question = "What does #{num1} plus #{num2} equal?"
+  end
+
+
+end
+
+question1 = Question.new
+puts question1.question
