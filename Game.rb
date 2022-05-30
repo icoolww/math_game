f65835bdaaa6c0dcaffc17cf7bd86ccf8dabd7c1
icoolww <williamwijaya.mail@gmail.com>
2022-05-30 20:50:38 +0000

    Adding Game

---
+++ Game.rb
@@ -0,0 +1,34 @@
+require './Player'
+
+class Game
+
+  attr_reader :player, :turn :game_over
+
+  def initialize(players)
+    @players = players
+    @turn = @players[0]
+    @game_over = false
+  end
+
+  # put players in an array, then decide when to switch turn
+  def new_turn?
+    if @turn == @players[0]
+      @turn = @players[1]
+    else
+      @turn = players[0]
+    end
+  end
+
+  def game_over? 
+    @players.each do |player|
+      @game_over = true if player.lives == 0
+    end 
+    @game_over
+  end
+
+  def current_score
+    puts "#{players[0]} : #{players[0].lives}/3 VS #{players[1]} : #{players[1].lives}/3"
+  end
+
+
+end
